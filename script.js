c7814cbe1d237b68f3815756cbe02f2bdaa96fa2
tzvik15 <tzvik15@yahoo.com>
2019-10-16 20:43:54 -0600

    linked js library that makes buttons dragable!

--- script.js
+++ script.js
@@ -73,3 +73,45 @@ $("#tom5").on("click", function() {
 $("#tom6").on("click", function() {
     audio17.play();
 })
+
+//js for the dragging of buttons
+
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    
+    
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p')
+
+      textEl && (textEl.textContent =
+        'moved a distance of ' +
+        (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                   Math.pow(event.pageY - event.y0, 2) | 0))
+          .toFixed(2) + 'px')
+    }
+  })
+  function dragMoveListener (event) {
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+  
+    // translate the element
+    target.style.webkitTransform =
+      target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)'
+  
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+  }
+  
+
