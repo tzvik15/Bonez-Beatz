80e76ce4f7c17bdc9db92d1ddf5c74c51811e5e3
heliumface770 <heliumface770@gmail.com>
2019-10-19 09:31:10 -0600

    added stop functionality

--- timing.js
+++ timing.js
@@ -7,47 +7,38 @@ var keyDown = false;
 var intervalTime = 1; //keeps record and playback times in sync
 var timerVar = 1000; //timer length (unit subject to change)
 var newSound = true;
+var stop = false;
 
 function record() {
   recording = true;
   x = [];
-  t = setInterval(function() {
+  t = setInterval(function() {      //reads keypresses and pushes value to array
+    
     if (keyDown === true && whichKey === 81) {
-      x.push("q");
-    }
+      x.push("q");}
     if (keyDown === true && whichKey === 87) {
-      x.push("w");
-    }
+      x.push("w");}
     if (keyDown === true && whichKey === 69) {
-      x.push("e");
-    }
+      x.push("e");}
     if (keyDown === true && whichKey === 65) {
-      x.push("a");
-    }
+      x.push("a");}
     if (keyDown === true && whichKey === 83) {
-      x.push("s");
-    }
+      x.push("s");}
     if (keyDown === true && whichKey === 68) {
-      x.push("d");
-    }
+      x.push("d");}
     if (keyDown === true && whichKey === 90) {
-      x.push("z");
-    }
+      x.push("z");}
     if (keyDown === true && whichKey === 88) {
-      x.push("x");
-    }
+      x.push("x");}
     if (keyDown === true && whichKey === 67) {
-      x.push("c");
-    }
+      x.push("c");}
     if (keyDown === false) {
-      x.push("0");
-    }
+      x.push("0");}
     timer++;
     if (timer >= timerVar) {
       stopTimers();
       recording = false;
-      timer = 0;
-    }
+      timer = 0;}
   }, intervalTime);
 }
 
@@ -58,44 +49,33 @@ function playback() {
       audio1.play();
     }
     if (x[y] == "w") {
-        audio2.play();
-      }
+        audio2.play();}
       if (x[y] == "e") {
-        audio3.play();
-      }
+        audio3.play();}
       if (x[y] == "a") {
-        audio4.play();
-      }
+        audio4.play();}
       if (x[y] == "s") {
-        audio5.play();
-      }
+        audio5.play();}
       if (x[y] == "d") {
-        audio6.play();
-      }
+        audio6.play();}
       if (x[y] == "z") {
-        audio7.play();
-      }
+        audio7.play();}
       if (x[y] == "x") {
-        audio8.play();
-      }
+        audio8.play();}
       if (x[y] == "c") {
-        audio9.play();
-      }
+        audio9.play();}
 
     if (x[y + 1] !== 0 && x[y + 1] !== x[y]) {
-      stopSound = true;
-    }
+      stopSound = true;}
 
     if (x[y + 1] == 0) {
-      stopSound = false;
-    }
+      stopSound = false;}
 
     if (stopSound === true) {
         var audioArray = [audio1, audio2, audio3, audio4, audio5, audio6, audio7, audio8, audio9];
         for (var i = 0; i < audioArray.length; i++){
       audioArray[i].pause();
-      audioArray[i].currentTime = 0.0;}
-    }
+      audioArray[i].currentTime = 0.0;}}
     y++;
     timer++;
     if (timer >= timerVar) {
@@ -141,3 +121,12 @@ $("#playback").on("click", function() {
     playback();
   }
 });
+
+$("#stop").on("click", function(){
+    stopTimers();
+    timer = 0;
+    y = 0;
+    recording = false;
+    timer = 0;
+    alert("stop = true");
+})
